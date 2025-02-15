977e2a081bd26a1e8d07a508ba06b1447c14f4eb
Danilly Condori <danilly7@gmail.com>
2025-02-15 18:40:27 +0100

    chore(frontend): more icons

--- frontend/src/components/ui/icons/WalletIcon.tsx
+++ frontend/src/components/ui/icons/WalletIcon.tsx
@@ -1,4 +1,4 @@
-export const HeartPulseIcon = ({ className = '' }: { className?: string }) => (
+export const WalletIcon = ({ className = '' }: { className?: string }) => (
     <svg
         className={`w-6 h-6 ${className}`}
         xmlns="http://www.w3.org/2000/svg"
@@ -11,7 +11,7 @@ export const HeartPulseIcon = ({ className = '' }: { className?: string }) => (
         strokeLinecap="round"
         strokeLinejoin="round"
     >
-        <path d="M19 14c1.49-1.46 3-3.21 3-5.5A5.5 5.5 0 0 0 16.5 3c-1.76 0-3 .5-4.5 2-1.5-1.5-2.74-2-4.5-2A5.5 5.5 0 0 0 2 8.5c0 2.3 1.5 4.05 3 5.5l7 7Z" />
-        <path d="M3.22 12H9.5l.5-1 2 4.5 2-7 1.5 3.5h5.27" />
+        <path d="M19 7V4a1 1 0 0 0-1-1H5a2 2 0 0 0 0 4h15a1 1 0 0 1 1 1v4h-3a2 2 0 0 0 0 4h3a1 1 0 0 0 1-1v-2a1 1 0 0 0-1-1" />
+        <path d="M3 5v14a2 2 0 0 0 2 2h15a1 1 0 0 0 1-1v-4" />
     </svg>
 );
