8770dd4cb4319127d4cfbe6ee8a8939545ffa093
Danilly Condori <danilly7@gmail.com>
2025-02-16 10:50:19 +0100

    fix(ui): error box updated

--- frontend/src/components/ui/error-box/index.tsx
+++ frontend/src/components/ui/error-box/index.tsx
@@ -6,7 +6,7 @@ interface ErrorBoxProps {
 
 const ErrorBox: React.FC<ErrorBoxProps> = ({ message }) => {
   return (
-    <div className="flex items-center justify-center min-h-screen"> {/* Centrado en toda la pantalla */}
+    <div className="flex items-start justify-center pt-20"> {/* Ajuste en Y con pt-20 */}
       <div className="relative inline-block border-4 border-black rounded-lg bg-red-500 text-white p-8 transition-all duration-300">
         <div className="flex flex-col items-center justify-center text-center">
           <span className="text-4xl sm:text-5xl lg:text-6xl font-bold">Error</span>
