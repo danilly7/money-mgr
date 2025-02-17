9adf58062cf30779e392a57b352dd6cd57081132
Danilly Condori <danilly7@gmail.com>
2025-02-17 12:43:58 +0100

    style(ui): Search button + updates

--- frontend/src/components/ui/balance-box/index.tsx
+++ frontend/src/components/ui/balance-box/index.tsx
@@ -8,7 +8,7 @@ interface BalanceBoxProps {
 const BalanceBox: React.FC<BalanceBoxProps> = ({ balance }) => {
   return (
     <div className="flex items-start justify-center py-4 m-4">
-      <div className="relative w-[28rem] h-52 bg-personalizedGreen border-4 border-black rounded-2xl flex items-center justify-center text-black p-8 transition-all duration-300 overflow-hidden hover:scale-105 hover:shadow-xl">
+      <div className="relative w-[28rem] h-52 bg-[#9D4EDD] border-4 border-black rounded-2xl flex items-center justify-center text-black p-8 transition-all duration-300 overflow-hidden hover:scale-105 hover:shadow-xl">
         <div className="flex flex-col items-center justify-center text-center">
           <p className="text-black text-xl font-bold">Balance</p>
           <ScaleIcon className='w-9 h-9' />
