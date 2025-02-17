afce2c7570589fd0ccaf83d81797322410912664
Danilly Condori <danilly7@gmail.com>
2025-02-17 11:33:57 +0100

    style(ui): Cancel button + updates

--- frontend/src/components/ui/Plus-btn/index.tsx
+++ frontend/src/components/ui/Plus-btn/index.tsx
@@ -4,7 +4,7 @@ export const PlusButton = ({ onClick, className = "" }: { onClick: () => void; c
     return (
         <button
             onClick={onClick}
-            className={`w-14 h-14 bg-personalizedPink text-black font-extrabold border-4 border-black text-2xl hover:bg-personalizedOrange rounded-full flex items-center justify-center transition-all duration-300 hover:-translate-y-1 hover:scale-105 ${className}`}
+            className={`w-14 h-14 bg-personalizedOrange text-black font-extrabold border-4 border-black text-2xl hover:bg-personalizedPink rounded-full flex items-center justify-center transition-all duration-300 hover:-translate-y-1 hover:scale-105 ${className}`}
             //classname al final por si el padre quiere ponerle classnames, de esta manera los acepta
             aria-label="Submit"
         >
