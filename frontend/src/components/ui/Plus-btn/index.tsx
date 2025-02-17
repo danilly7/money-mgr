4c1c9530258525390f3d251e06505e9f90de5c84
Danilly Condori <danilly7@gmail.com>
2025-02-17 16:10:52 +0100

    fix(ui): Plus button navigates now

--- frontend/src/components/ui/Plus-btn/index.tsx
+++ frontend/src/components/ui/Plus-btn/index.tsx
@@ -1,12 +1,20 @@
+import { useNavigate } from "react-router-dom";
 import { PlusIcon } from "../icons/PlusIcon";
 
-export const PlusButton = ({ onClick, className = "" }: { onClick: () => void; className?: string }) => {
+export const PlusButton = ({ to, className = "" }: { to: string; className?: string }) => {
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        navigate(to);
+    };
+
     return (
         <button
-            onClick={onClick}
-            className={`w-14 h-14 bg-personalizedPink text-black font-extrabold border-4 border-black text-2xl hover:bg-fuchsia-500 rounded-full flex items-center justify-center transition-all duration-300 hover:-translate-y-1 hover:scale-105 ${className}`}
-            //classname al final por si el padre quiere ponerle classnames, de esta manera los acepta
-            aria-label="Submit"
+            onClick={handleClick}
+            className={`w-14 h-14 bg-personalizedPink text-black font-extrabold border-4 border-black text-2xl 
+            hover:bg-fuchsia-500 rounded-full flex items-center justify-center transition-all duration-300 
+            hover:-translate-y-1 hover:scale-105 ${className}`}
+            aria-label="Add"
         >
             <PlusIcon />
         </button>
