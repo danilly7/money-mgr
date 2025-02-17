ff9473207b281034ade2f9698b95b01dcdc1b62b
Danilly Condori <danilly7@gmail.com>
2025-02-17 20:07:15 +0100

    feat(frontend): Home page part 1 done

--- frontend/src/pages/home/index.tsx
+++ frontend/src/pages/home/index.tsx
@@ -1,20 +1,28 @@
+import { useState } from "react";
+import SwitchTimeframe from "../../components/ui/switch-time-frame";
+import BalanceBox from "../../components/ui/balance-box";
+import ExpensesBox from "../../components/ui/expenses-box";
+import IncomeBox from "../../components/ui/income-box";
+import { SearchButton } from "../../components/ui/search-btn";
+import { PlusButton } from "../../components/ui/plus-btn";
+
 const Home = () => {
-    const loremText = "Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque, sint nisi consequatur error eveniet, similique temporibus, eius accusamus est voluptas quos velit laboriosam modi ut expedita doloribus incidunt veritatis. Veritatis.";
+    const [timeframe, setTimeframe] = useState<"Day" | "Week" | "Month" | "Year">("Month");
 
-    const repeatCount = 40;
-
-    const repeatedText = new Array(repeatCount).fill(loremText);
 
     return (
-        <div className="flex flex-col items-center justify-center p-4">
-            <h1 className="text-4xl font-bold">Welcome to Money Manager</h1>
-
-            <p className="text-lg">Manage your money in a simple way</p>
-            
-            {repeatedText.map((text, index) => (
-                <p key={index}>{text}</p>
-            ))}
-        </div>
+        <>
+            <SwitchTimeframe timeframe={timeframe} setTimeframe={setTimeframe} />
+            <BalanceBox balance={1150.5} />
+            <div className="flex flex-col justify-center sm:flex-row">
+                <ExpensesBox expenses={100.00} />
+                <IncomeBox income={500} />
+            </div>
+            <div className="flex flex-col justify-center sm:flex-row space-x-8 m-6">
+                <SearchButton to="/details" />
+                <PlusButton to="/newaccount" />
+            </div>
+        </>
     );
 };
 
