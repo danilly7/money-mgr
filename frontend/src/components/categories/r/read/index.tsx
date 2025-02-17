9c16aed66065f6b13834b2723baed6507cd2ce68
Danilly Condori <danilly7@gmail.com>
2025-02-17 11:13:51 +0100

    style(ui):

--- frontend/src/components/categories/r/read/index.tsx
+++ frontend/src/components/categories/r/read/index.tsx
@@ -21,20 +21,23 @@ export const CategoriesList: React.FC<CategoriesListProps> = ({ isExpense }) => 
 
     return (
         <div className="flex justify-center">
-            <div className="flex flex-wrap justify-center m-8 gap-x-6 gap-y-4 max-w-[80%]">
+            <div className="flex flex-wrap justify-center m-6 gap-x-4 gap-y-4 max-w-[80%]">
                 {filteredCategories.map((category) => {
                     const Icon = category.icon;
 
                     return (
                         <div
                             key={category.id}
-                            className="group flex items-center justify-center w-36 h-36 rounded-full border-4 border-black text-center transition-all duration-300 hover:rotate-3 hover:scale-110 hover:shadow-lg hover:bg-opacity-90"
+                            className="group flex items-center justify-center 
+                                       w-32 h-32 lg:w-36 lg:h-36
+                                       rounded-full border-4 border-black text-center
+                                       transition-all duration-300 hover:rotate-3 hover:scale-110 hover:shadow-lg hover:bg-opacity-90"
                             style={{ backgroundColor: category.color.color }}
                         >
                             <div className="flex flex-col items-center justify-center">
-                                <Icon className="w-9 h-9 text-black transition-all duration-300" />
+                                <Icon className="w-7 h-7 lg:w-9 lg:h-9 text-black transition-all duration-300" />
                                 
-                                <strong className="text-lg opacity-0 transition-all duration-300 group-hover:opacity-100">
+                                <strong className="text-sm lg:text-lg opacity-0 transition-all duration-300 group-hover:opacity-100">
                                     {category.name}
                                 </strong>
                             </div>
