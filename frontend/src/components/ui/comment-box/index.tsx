d65651df6a812b5abc282fd6f07f9e6df92d9547
Danilly Condori <danilly7@gmail.com>
2025-02-26 00:35:41 +0100

    fix(ui): name change: description box to comment box

--- frontend/src/components/ui/comment-box/index.tsx
+++ frontend/src/components/ui/comment-box/index.tsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from 'react';
 
-interface DescriptionBoxProps {
-  initialDescription: string;
-  onDescriptionChange: (description: string) => void;
+interface CommentBoxProps {
+  initialComment: string;
+  onCommentChange: (comment: string) => void;
 }
 
-export const DescriptionBox: React.FC<DescriptionBoxProps> = ({ initialDescription, onDescriptionChange }) => {
-  const [description, setDescription] = useState<string>(initialDescription);
+export const CommentBox: React.FC<CommentBoxProps> = ({ initialComment, onCommentChange }) => {
+  const [comment, setComment] = useState<string>(initialComment);
   const [isEditing, setIsEditing] = useState<boolean>(false);
 
   useEffect(() => {
-    setDescription(initialDescription);
-  }, [initialDescription]);
+    setComment(initialComment);
+  }, [initialComment]);
 
   const handleClick = () => {
     setIsEditing(true);
@@ -19,16 +19,16 @@ export const DescriptionBox: React.FC<DescriptionBoxProps> = ({ initialDescripti
 
   const handleBlur = () => {
     setIsEditing(false);
-    onDescriptionChange(description);
+    onCommentChange(comment);
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-    setDescription(event.target.value);
+    setComment(event.target.value);
   };
 
   const handleKeyDown = (event: React.KeyboardEvent<HTMLInputElement | HTMLTextAreaElement>) => {
     if (event.key === 'Enter') {
-      event.preventDefault(); //para que no haga el salto de linea ya que es un Enter
+      event.preventDefault(); // Evita el salto de línea al presionar Enter
       handleBlur();
     }
   };
@@ -43,26 +43,26 @@ export const DescriptionBox: React.FC<DescriptionBoxProps> = ({ initialDescripti
           <p
             className={`text-black text-xl font-bold transition-all duration-300 ${isEditing ? 'transform -translate-y-6 text-2xl' : ''}`}
           >
-            Description
+            Comment
           </p>
 
           {isEditing ? (
             <input
               type="text"
-              value={description}
+              value={comment}
               onChange={handleChange}
               onBlur={handleBlur}
               onKeyDown={handleKeyDown}
               className="text-4xl font-bold text-center bg-transparent border-none outline-none"
               autoFocus
-              placeholder="Blablabl"
+              placeholder="Write a comment..."
               inputMode="text"
             />
           ) : (
-            <span className="text-4xl font-bold mt-3 text-center">{description}</span>
+            <span className="text-4xl font-bold mt-3 text-center">{comment}</span>
           )}
 
-          {!isEditing && description === "" && <div className="w-[calc(100%+7rem)] border-b-4 border-black mt-8" />}
+          {!isEditing && comment === "" && <div className="w-[calc(100%+7rem)] border-b-4 border-black mt-8" />}
         </div>
       </div>
     </div>
