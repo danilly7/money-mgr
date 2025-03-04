cc736d7b916875c6b5ad49d112632bb3ecae99c8
Danilly Condori <danilly7@gmail.com>
2025-03-05 00:18:35 +0100

    fix(frontend): updated env variables in apis

--- frontend/src/api/index.tsx
+++ frontend/src/api/index.tsx
@@ -1,9 +1,9 @@
-export const apiUsers = `${process.env.REACT_APP_API_URL}/api/users`;
+export const apiUsers = `${import.meta.env.VITE_REACT_APP_API_URL}/api/users`;
 
-export const apiAccounts = `${process.env.REACT_APP_API_URL}/api/accounts`;
+export const apiAccounts = `${import.meta.env.VITE_REACT_APP_API_URL}/api/accounts`;
 
-export const apiCategories = `${process.env.REACT_APP_API_URL}/api/categories`;
+export const apiCategories = `${import.meta.env.VITE_REACT_APP_API_URL}/api/categories`;
 
-export const apiTransactions = `${process.env.REACT_APP_API_URL}/api/transactions`;
+export const apiTransactions = `${import.meta.env.VITE_REACT_APP_API_URL}/api/transactions`;
 
-export const apiTransfers = `${process.env.REACT_APP_API_URL}/api/transfers`;
+export const apiTransfers = `${import.meta.env.VITE_REACT_APP_API_URL}/api/transfers`;
