0298d37c485ed2efe405cf9a3b900622ec74794a
Danilly Condori <danilly7@gmail.com>
2025-03-05 10:55:22 +0100

    fix(ui): update Add button

--- frontend/src/components/ui/add-button/index.tsx
+++ frontend/src/components/ui/add-button/index.tsx
@@ -15,15 +15,15 @@ const AddButton: React.FC<AddButtonProps> = ({ to, className = "" }) => {
   };
 
   return (
-    <div className={`w-[80%] flex justify-center ${className}`}>
-      <button
-        onClick={handleClick}
-        className="w-full max-w-[60px] h-[60px] bg-personalizedPink text-black font-extrabold border-4 border-black text-2xl flex items-center justify-center rounded-full transition-all duration-300 hover:scale-105 hover:shadow-md cursor-pointer"
-        aria-label="Add"
-      >
-        <PlusIcon />
-      </button>
-    </div>
+    <button
+      onClick={handleClick}
+      className={`w-14 h-14 bg-personalizedPink text-black font-extrabold border-4 border-black text-2xl 
+                  flex items-center justify-center rounded-full transition-all duration-300 
+                  hover:scale-105 hover:shadow-md cursor-pointer ${className}`}
+      aria-label="Add"
+    >
+      <PlusIcon />
+    </button>
   );
 };
 
