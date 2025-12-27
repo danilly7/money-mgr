fd38ea5c0bc1a4b95f9897410091ee625fc8512c
danilly7 <danilly7@gmail.com>
2025-12-28 00:09:25 +0100

    style: :lipstick: user profile colors changed

--- frontend/src/pages/user/index.tsx
+++ frontend/src/pages/user/index.tsx
@@ -25,20 +25,52 @@ const User = () => {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center h-screen mb-2">
-            <UserIcon className="w-9 h-9 text-personalizedPurple hover:text-personalizedOrange"/>
-            <h1 className="text-4xl font-bold">User</h1>
-            <div className="m-8 text-center text-lg">
-                <p><strong>Email:</strong> {currentUser?.email}</p>
-                <p><strong>Name:</strong> {currentUser?.displayName}</p>
+        <div className="flex flex-col items-center justify-center h-screen">
+            <div
+                className="group bg-white border-4 border-black rounded-lg shadow-lg px-6 py-8 text-center w-96"
+            >
 
-            </div>
+                <UserIcon
+                    className="w-10 h-10 mx-auto mt-8 mb-4 
+                           text-personalizedPurple 
+                           transition-colors duration-300"
+                />
+
+                <h1 className="text-3xl font-bold mb-14 text-personalizedPurple">You is...</h1>
+
+                <div className="text-lg mb-6">
+                    <p><strong>Name:</strong> {currentUser?.displayName}</p>
+                    <p><strong>Email:</strong> {currentUser?.email}</p>
+                </div>
+
+                <h2
+                    className="text-3xl font-bold mt-8 
+                    opacity-0 translate-y-2
+                    transition-all duration-300
+                    group-hover:opacity-100 group-hover:translate-y-0 text-personalizedOrange"
+                >
+                    ...smart,
+                </h2>
+                <h2
+                    className="text-3xl font-bold mb-4
+                    opacity-0 translate-y-2
+                    transition-all duration-300 delay-75
+                    group-hover:opacity-100 group-hover:translate-y-0 text-personalizedOrange"
+                >
+                    you is kind.
+                </h2>
+
                 <button
                     onClick={handleLogout}
-                    className="mt-4 px-8 py-4 text-xl font-semibold bg-personalizedPurple text-white border-4 border-black rounded-lg transition-all duration-300 hover:text-black hover:bg-personalizedOrange"
+                    className="px-6 py-3 my-8 text-lg font-semibold 
+                           bg-personalizedPurple text-white 
+                           border-4 border-black rounded-lg 
+                           transition-all duration-300 
+                           hover:text-black hover:bg-personalizedOrange"
                 >
                     Logout
                 </button>
+            </div>
         </div>
     );
 };
